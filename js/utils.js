19c6b0ab20bfeda2019733cd168ac4219395f349
PARK MIYEON <miyoun1003@naver.com>
2023-05-09 19:48:43 +0900

    클릭하거나 마우스 올리면 바뀌는 거

--- js/utils.js
+++ js/utils.js
@@ -17,7 +17,8 @@ function total(x, y){
 }
 
 function name(){
-    let y=prompt("이름");
+    // let y=prompt("이름");
+    const y = prompt("이름");
     while(y === null || y === ""){
         alert("입력해주세요");
         y=prompt("이름");
@@ -26,4 +27,19 @@ function name(){
         console.log(y);
         alert(y+"님 안녕하세요");
     
+}
+
+function checkNum(){
+    let num = prompt("숫자");
+    let bool = !isNaN(num);
+    if(!bool){
+        alert("숫자를 입력해 주세요");
+    } else if(num < 1 || num > 100){
+        alert("1이상 100이하를 넣어주세요");
+    } else {
+        for(let i = 1; i <= 100; i++){
+            console.log(i);
+        }
+    }
+
 }
